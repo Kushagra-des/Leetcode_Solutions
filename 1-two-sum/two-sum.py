936e56706c694d2844f30adb0b58b852431d8f7b
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-08-06 23:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (40.54%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in mp:
+                return [mp[complement], i]
+            mp[num] = i
+
+# Complexity
+# Time: O(n)
+# Space: O(n)
