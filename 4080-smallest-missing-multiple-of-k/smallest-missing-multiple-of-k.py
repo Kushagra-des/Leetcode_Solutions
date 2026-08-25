5444b1248c63ff37436c457a227aa3c456e0b1bc
Kushagra Sharma <kushagra.sharma595@gmail.com>
2026-08-25 21:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.61%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        seen = set(nums)
+
+        x = k
+        while x in seen:
+            x += k
+
+        return x
